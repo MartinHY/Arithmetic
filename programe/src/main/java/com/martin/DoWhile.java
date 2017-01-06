6b1ee0da373068f0b6dbcc5140250b7a9669fe6c
MartinBZDQSM <569835826@qq.com>
2017-01-06 18:55:10 +0800

    basic

---
+++ programe/src/main/java/com/martin/DoWhile.java
@@ -0,0 +1,40 @@
+package com.martin;
+
+/**
+ * 作者：MartinBZDQSM on 2016/9/13 0013.
+ * 博客：http://www.jianshu.com/users/78f0e5f4a403/latest_articles
+ * github：https://github.com/MartinBZDQSM
+ * <p>
+ * 区分do-while 和 while-do 的区别
+ */
+public class DoWhile {
+
+
+    public static void main(String[] args) {
+
+        doWhile(0);
+        whileDo(0);
+    }
+
+
+    /**
+     * 两个方法的主要区别在于：do-while 不管条件与否都会先执行一次
+     * 而while-do需要判断完全条件再执行
+     */
+
+    public static void doWhile(int i) {
+        do {
+            i--;
+            System.out.println("doWhile");
+        } while (i > 0);
+    }
+
+    public static void whileDo(int i) {
+        while (i > 0) {
+            i--;
+            System.out.println("whileDo");
+        }
+    }
+
+
+}
---
+++ programe/src/main/java/com/martin/Spicniess.java
@@ -0,0 +1,13 @@
+package com.martin;
+
+/**
+ * 作者：MartinBZDQSM on 2016/9/13 0013.
+ * 博客：http://www.jianshu.com/users/78f0e5f4a403/latest_articles
+ * github：https://github.com/MartinBZDQSM
+ */
+public enum Spicniess {
+
+    NOT,MLID,MEDIUM,HOT,FLAMING;
+
+
+}
